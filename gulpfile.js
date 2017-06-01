99f86e2bfaff1cb2107e9730be4e656db761c38e
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-06-01 12:59:08 +0530

    Gulp app first commit

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+gulp.task('task', function(){
+	//do something
+	console.log('Hey there! Gulp is here now B-)');
+});
+
+gulp.task('task2', function(){
+	//do something
+	console.log('Ohh yeah now we are talking');
+});
