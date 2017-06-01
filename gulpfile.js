6fee226fd0624e5a56f5dafa3f5b567e159f592c
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-06-01 13:15:46 +0530

    Converted scss to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
-gulp.task('task', function(){
-	//do something
-	console.log('Hey there! Gulp is here now B-)');
-});
+var sass = require('gulp-sass');
 
-gulp.task('task2', function(){
-	//do something
-	console.log('Ohh yeah now we are talking');
+// gulp.task('task', function(){
+// 	//do something
+// 	console.log('Hey there! Gulp is here now B-)');
+// });
+
+// gulp.task('task2', function(){
+// 	//do something
+// 	console.log('Ohh yeah now we are talking');
+// });
+
+gulp.task('sass', function(){
+	return gulp.src('app/scss/styles.scss').pipe(sass())
+		.pipe(gulp.dest('app/css'));
 });
